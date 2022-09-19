2f78b3bb351a9a547b16d2c07f5fb77b0e4d391a
2019182033JCH <jangsy1050@naver.com>
2022-09-19 17:19:00 +0900

    drill5

--- drill5.py
+++ drill5.py
@@ -1,8 +1,46 @@
 Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
+idle
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    idle
+NameError: name 'idle' is not defined
+>>> import turtle
 >>> 
-=============== RESTART: C:/Users/jangs/OneDrive/바탕 화면/drill5.py ===============
+>>> def moveup():
+...     turtle.setheading(90)
+...     turtle.forward(50)
+...     turtle.stamp()
+... 
+...     
+>>> def movedown():
+...     turtle.setheading(-90)
+...     turtle.forward(50)
+...     turtle.stamp()
+... 
+...     
+>>> def moveleft():
+...     turtle.setheading(180)
+...     turtle.forward(50)
+...     turtle.stamp()
+... 
+...     
+>>> def moveright():
+...     turtle.setheading(0)
+...     turtle.forward(50)
+...     turtle.stamp()
+... 
+...     
+>>> turtle.shape('turtle')
 >>> 
-=============== RESTART: C:/Users/jangs/OneDrive/바탕 화면/drill5.py ===============
+>>> def restart():
+...     turtle.reset()
+... 
+...     
+>>> turtle.onkey(moveup, 'w')
+>>> turtle.onkey(movedown, 's')
+>>> turtle.onkey(moveleft, 'a')
+>>> turtle.onkey(moveright, 'd')
+>>> turtle.onkey(restart, 'Escape')
+>>> turtle.listen()
 >>> 
-=============== RESTART: C:/Users/jangs/OneDrive/바탕 화면/drill5.py ===============
